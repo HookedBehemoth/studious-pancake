dec9ab2f2bdb2037517f62456c1c0255aa4001cd
HookedBehemoth <luisscheurenbrand@gmail.com>
2023-01-22 23:18:59 +0100

    bump copyright year to 2023

--- applet/main.cpp
+++ applet/main.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2021 Studious Pancake
+ * Copyright (c) 2020-2023 Studious Pancake
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
